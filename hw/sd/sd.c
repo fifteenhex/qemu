f62249d0b0942dae6e957741c1815cf12df5cf20
Daniel Palmer <daniel@thingy.jp>
2025-04-12 08:50:03 +0900

    sd spi hacks

--- hw/sd/sd.c
+++ hw/sd/sd.c
@@ -1575,9 +1575,9 @@ static sd_rsp_type_t sd_cmd_STOP_TRANSMISSION(SDState *sd, SDRequest req)
 /* CMD13 */
 static sd_rsp_type_t sd_cmd_SEND_STATUS(SDState *sd, SDRequest req)
 {
-    if (sd->mode != sd_data_transfer_mode) {
-        return sd_invalid_mode_for_cmd(sd, req);
-    }
+    //if (sd->mode != sd_data_transfer_mode) {
+    //    return sd_invalid_mode_for_cmd(sd, req);
+    //}
 
     switch (sd->state) {
     case sd_standby_state:
@@ -1592,7 +1592,7 @@ static sd_rsp_type_t sd_cmd_SEND_STATUS(SDState *sd, SDRequest req)
     }
 
     if (sd_is_spi(sd)) {
-        return sd_r2_s;
+        return sd_r1;
     }
 
     return sd_req_rca_same(sd, req) ? sd_r1 : sd_r0;
