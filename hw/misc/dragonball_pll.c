f114cb8853dbb4647895d9cd9f071cf4201afe5a
Daniel Palmer <daniel@thingy.jp>
2025-12-10 17:21:12 +0900

    db pll

--- hw/misc/dragonball_pll.c
+++ hw/misc/dragonball_pll.c
@@ -112,7 +112,7 @@ static const VMStateDescription vmstate_dragonball_pll = {
 //    DEFINE_PROP_END_OF_LIST(),
 //};
 
-static void dragonball_pll_class_init(ObjectClass *klass, void *data)
+static void dragonball_pll_class_init(ObjectClass *klass, const void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
