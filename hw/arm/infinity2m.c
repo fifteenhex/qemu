fc1d150764144438b650834c2b23bfd2223d2fa2
Daniel's intern <intern@thingy.jp>
2026-07-19 14:36:39 +0000

    hw/arm: Add infinity2m SoC family
    The infinity2m family of MStar ARMv7 SoCs are dual Cortex-A7 parts
    with DRAM bonded into the package. The known members, SSD201 and
    SSD202D, differ only in the amount and type of that DRAM. Add an
    abstract subclass of the MStar ARMv7 base class for the family; it
    sets the core count and will grow the hardware blocks that are
    common to the family but not the whole range.
    
    Signed-off-by: Daniel's intern <intern@thingy.jp>

---
+++ hw/arm/infinity2m.c
@@ -0,0 +1,34 @@
+/*
+ * MStar/SigmaStar infinity2m SoC family
+ *
+ * The infinity2m family are dual Cortex-A7 SoCs with in-package DRAM.
+ * Known members are the SSD201 and the SSD202D, which differ only in
+ * the amount and type of DRAM bonded into the package.
+ *
+ * Copyright (c) 2026 Daniel Palmer <daniel@thingy.jp>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include "hw/arm/infinity2m.h"
+
+static void infinity2m_soc_class_init(ObjectClass *oc, const void *data)
+{
+    MStarV7SoCClass *msc = MSTARV7_SOC_CLASS(oc);
+
+    msc->num_cpus = INFINITY2M_NUM_CPUS;
+}
+
+static const TypeInfo infinity2m_soc_types[] = {
+    {
+        .name           = TYPE_INFINITY2M_SOC,
+        .parent         = TYPE_MSTARV7_SOC,
+        .instance_size  = sizeof(Infinity2MSoCState),
+        .class_size     = sizeof(Infinity2MSoCClass),
+        .class_init     = infinity2m_soc_class_init,
+        .abstract       = true,
+    },
+};
+
+DEFINE_TYPES(infinity2m_soc_types)
---
+++ include/hw/arm/infinity2m.h
@@ -0,0 +1,32 @@
+/*
+ * MStar/SigmaStar infinity2m SoC family
+ *
+ * Copyright (c) 2026 Daniel Palmer <daniel@thingy.jp>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_ARM_INFINITY2M_H
+#define HW_ARM_INFINITY2M_H
+
+#include "hw/arm/mstarv7.h"
+
+#define TYPE_INFINITY2M_SOC "infinity2m-soc"
+OBJECT_DECLARE_TYPE(Infinity2MSoCState, Infinity2MSoCClass, INFINITY2M_SOC)
+
+/* All infinity2m SoCs are dual core */
+#define INFINITY2M_NUM_CPUS 2
+
+struct Infinity2MSoCState {
+    /*< private >*/
+    MStarV7SoCState parent_obj;
+    /*< public >*/
+};
+
+struct Infinity2MSoCClass {
+    /*< private >*/
+    MStarV7SoCClass parent_class;
+    /*< public >*/
+};
+
+#endif /* HW_ARM_INFINITY2M_H */
